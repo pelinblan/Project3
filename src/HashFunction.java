e93128785b362b1b95712f85255cc92f39627f95
pelinblanton <blanpe22@wfu.edu>
2023-11-29 15:33:57 -0600

    most recent 2

--- src/HashFunction.java
+++ src/HashFunction.java
@@ -9,18 +9,19 @@ public class HashFunction implements HashTable<String, HashObject> {
     // using folding on a string, summed 4 bytes at a time
     private long sfold(String s, int M) {
         long sum = 0, mul = 1;
-        for(int i = 0; i < s.length(); i++){
-            mul =  (i % 4 == 0) ? 1 : mul * 256;
+        for (int i = 0; i < s.length(); i++) {
+            mul = (i % 4 == 0) ? 1 : mul * 256;
             sum += s.charAt(i) * mul;
         }
         return (int) (Math.abs(sum) % M);
     }
 
-
+    //returns the hash value for a given ID
     public int hash(String id) {
         return (int) sfold(id, hashes.length);
     }
 
+    //inserts a HashObject into the hash table
     public int insert(String id, String seq) {
         HashObject hashObject = new HashObject(id, seq);
         int hashValue = hash(id);
@@ -39,12 +40,11 @@ public class HashFunction implements HashTable<String, HashObject> {
         return -1;
     }
 
-
+    //removes a HashObject from the hash table
     public HashObject remove(String id) {
         int hashValue = hash(id);
-        int indexStart = hashValue;
 
-        do {
+        for (int i = 0; i < hashes.length; i++) {
             if (hashes[hashValue] != null && hashes[hashValue].getId().equals(id)) {
                 HashObject removedObject = hashes[hashValue];
                 hashes[hashValue] = null;
@@ -52,52 +52,45 @@ public class HashFunction implements HashTable<String, HashObject> {
             }
 
             hashValue = (hashValue + 1) % hashes.length; // Linear probing pos = (home + p(k, i)) % M
+        }
 
-        } while (hashValue != indexStart);
-
-        // HashObject not found
+        //not found
         System.out.println("Error: Sequence not found.");
         return null;
     }
 
-
+    // returns an array of non-null HashObjects in the hash table
     public HashObject[] print() {
         int notNull = 0;
 
-      //if the object is not empty, count it
+        //if the object is not empty, count it
         for (HashObject hashObject : hashes) {
             if (hashObject != null) {
                 notNull++;
             }
         }
-
         HashObject[] result = new HashObject[notNull];
         int index = 0;
-
         //create result array
         for (HashObject hashObject : hashes) {
             if (hashObject != null) {
                 result[index++] = hashObject;
             }
         }
-
         return result;
     }
-
+    //searches for a HashObject in the hash table
     public HashObject search(String id) {
         int hashValue = hash(id);
-        int initialIndex = hashValue;
 
-        do {
+        for (int i = 0; i < hashes.length; i++) {
             if (hashes[hashValue] != null && hashes[hashValue].getId().equals(id)) {
-                return hashes[hashValue]; //found the object
+                return hashes[hashValue]; // found the object
             }
 
             hashValue = (hashValue + 1) % hashes.length; // Linear probing
-
-        } while (hashValue != initialIndex);
-
-        //object not found
+        }
+        // object not found
         return null;
     }
 }
